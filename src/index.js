bff9c3d2bc17adaf484e576b14105099c11a6e30
BenjyC <ben.coler97@gmail.com>
2019-07-25 22:22:45 +0100

    added styling to show board and default yellow counters

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ function Square(props) {
     );
 
 }
+
+function Circle(props) {
+
+    return (
+        <button className="circle">
+            {props.value}
+        </button>
+    );
+
+}
   
 class Board extends React.Component {
 
@@ -18,18 +28,28 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(16).fill(null),
+            circles: Array(16).fill(null),
             yIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-
+        
         squares[i] = this.state.yIsNext ? 'Red':'Yellow';
         this.setState({
             sqaures: squares,
             yIsNext: !this.state.yIsNext,
         });
+    }
+
+    renderCircle(i) {
+        return (
+            <Circle
+                // value={this.state.circles[i]}
+                // onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     renderSquare(i) {
@@ -47,31 +67,64 @@ class Board extends React.Component {
         return (
             <div>
                 <div className="status">{status}</div>
+                <div class="container_row">
+                    <div class="fullboard">
+                        <div className="board-row">
+                            {this.renderSquare(0)}
+                            {this.renderSquare(1)}
+                            {this.renderSquare(2)}
+                            {this.renderSquare(3)}
+                        </div>
+                        <div className="board-row">
+                            {this.renderSquare(4)}
+                            {this.renderSquare(5)}
+                            {this.renderSquare(6)}
+                            {this.renderSquare(7)}
+                        </div>
+                        <div className="board-row">
+                            {this.renderSquare(8)}
+                            {this.renderSquare(9)}
+                            {this.renderSquare(10)}
+                            {this.renderSquare(11)}
+                        </div>
+                        <div className="board-row">
+                            {this.renderSquare(12)}
+                            {this.renderSquare(13)}
+                            {this.renderSquare(14)}
+                            {this.renderSquare(15)}
+                        </div>
+                    </div>
+                    
+
+                    <div class="fullcounters">
+                        <div className="counter-row">
+                            {this.renderCircle(0)}
+                            {this.renderCircle(1)}
+                            {this.renderCircle(2)}
+                            {this.renderCircle(3)}
+                        </div>
+                        <div className="counter-row">
+                            {this.renderCircle(4)}
+                            {this.renderCircle(5)}
+                            {this.renderCircle(6)}
+                            {this.renderCircle(7)}
+                        </div>
+                        <div className="counter-row">
+                            {this.renderCircle(8)}
+                            {this.renderCircle(9)}
+                            {this.renderCircle(10)}
+                            {this.renderCircle(11)}
+                        </div>
+                        <div className="counter-row">
+                            {this.renderCircle(12)}
+                            {this.renderCircle(13)}
+                            {this.renderCircle(14)}
+                            {this.renderCircle(15)}
+                        </div>
+                    </div>
+                    
+                </div>
                 
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                    {this.renderSquare(3)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(8)}
-                    {this.renderSquare(9)}
-                    {this.renderSquare(10)}
-                    {this.renderSquare(11)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(12)}
-                    {this.renderSquare(13)}
-                    {this.renderSquare(14)}
-                    {this.renderSquare(15)}
-                </div>
             </div>
         );
     }
