1ca2b54d610147bd2e034ecce5e372a5651424e4
BenjyC <ben.coler97@gmail.com>
2019-07-26 17:45:48 +0100

    increased board size & added winner function

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-
     return (
         <button className="square" onClick={props.onClick}>
             {props.value}
         </button>
     );
-
 }
 
 function Circle(props) {
@@ -23,12 +21,11 @@ function Circle(props) {
 }
   
 class Board extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(16).fill(null),
-            circles: Array(16).fill(null),
+            squares: Array(42).fill(null),
+            //circles: Array(16).fill(null),
             yIsNext: true,
         };
     }
@@ -38,7 +35,7 @@ class Board extends React.Component {
         
         squares[i] = this.state.yIsNext ? 'Red':'Yellow';
         this.setState({
-            sqaures: squares,
+            squares: squares,
             yIsNext: !this.state.yIsNext,
         });
     }
@@ -62,7 +59,15 @@ class Board extends React.Component {
     }   
 
     render() {
-        const status = 'Next player: ' + (this.state.yIsNext ? 'Red':'Yellow');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next Player: ' + (this.state.yIsNext ? 'Red':'Yellow');
+        }
+
 
         return (
             <div>
@@ -74,29 +79,58 @@ class Board extends React.Component {
                             {this.renderSquare(1)}
                             {this.renderSquare(2)}
                             {this.renderSquare(3)}
-                        </div>
-                        <div className="board-row">
                             {this.renderSquare(4)}
                             {this.renderSquare(5)}
                             {this.renderSquare(6)}
-                            {this.renderSquare(7)}
                         </div>
                         <div className="board-row">
+                            {this.renderSquare(7)}
                             {this.renderSquare(8)}
                             {this.renderSquare(9)}
                             {this.renderSquare(10)}
                             {this.renderSquare(11)}
-                        </div>
-                        <div className="board-row">
                             {this.renderSquare(12)}
                             {this.renderSquare(13)}
+                        </div>
+                        <div className="board-row">
                             {this.renderSquare(14)}
                             {this.renderSquare(15)}
+                            {this.renderSquare(16)}
+                            {this.renderSquare(17)}
+                            {this.renderSquare(18)}
+                            {this.renderSquare(19)}
+                            {this.renderSquare(20)}
+                        </div>
+                        <div className="board-row">
+                            {this.renderSquare(21)}
+                            {this.renderSquare(22)}
+                            {this.renderSquare(23)}
+                            {this.renderSquare(24)}
+                            {this.renderSquare(25)}
+                            {this.renderSquare(26)}
+                            {this.renderSquare(27)}
+                        </div>
+                        <div className="board-row">
+                            {this.renderSquare(28)}
+                            {this.renderSquare(29)}
+                            {this.renderSquare(30)}
+                            {this.renderSquare(31)}
+                            {this.renderSquare(32)}
+                            {this.renderSquare(33)}
+                            {this.renderSquare(34)}
+                        </div>
+                        <div className="board-row">
+                            {this.renderSquare(35)}
+                            {this.renderSquare(36)}
+                            {this.renderSquare(37)}
+                            {this.renderSquare(38)}
+                            {this.renderSquare(39)}
+                            {this.renderSquare(40)}
+                            {this.renderSquare(41)}
                         </div>
                     </div>
                     
-
-                    <div class="fullcounters">
+                    {/* <div class="fullcounters">
                         <div className="counter-row">
                             {this.renderCircle(0)}
                             {this.renderCircle(1)}
@@ -121,10 +155,8 @@ class Board extends React.Component {
                             {this.renderCircle(14)}
                             {this.renderCircle(15)}
                         </div>
-                    </div>
-                    
+                    </div> */}
                 </div>
-                
             </div>
         );
     }
@@ -145,6 +177,20 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2, 3],
+        [3, 4, 5, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c, d] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
   
 // ========================================
